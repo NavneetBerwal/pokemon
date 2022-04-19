e27286c2cc8398e7a368d79c634e34a5945d539b
likhitha-reddy <likhitha0341@gmail.com>
2022-04-19 19:35:20 +0530

    first commit

---
+++ src/Components/Description.js
@@ -0,0 +1,18 @@
+import React from 'react'
+const Description=({heightp,weightp,pokstat1,pokstat2,pokstat3,pokstat4,pokstat5,pokstat6,posbs1,posbs2,posbs3,posbs4,posbs5,posbs6}) => 
+{
+    return(
+        <div className='desc'>
+            <p><b>HEIGHT IS {heightp*10}</b>cm</p>
+            <p><b>WEIGHT IS {weightp*10}</b>cm</p>
+            <h3>Stat</h3>
+            <p><b>{pokstat1} : {posbs1}</b></p>
+            <p><b>{pokstat2} : {posbs2}</b></p>
+            <p><b>{pokstat3} : {posbs3}</b></p>
+            <p><b>{pokstat4} : {posbs4}</b></p>
+            <p><b>{pokstat5} : {posbs5}</b></p>
+            <p><b>{pokstat6} : {posbs6}</b></p>
+        </div>
+    )
+}
+export default Description
